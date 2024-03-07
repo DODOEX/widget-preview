21c44a7fb6161c4294a7e51d3a99b93f0e6341a7
junjie <wangjunjie0817@foxmail.com>
2024-03-07 12:03:48 +0800

    feat: optimization title

--- app/[id]/layout.tsx
+++ app/[id]/layout.tsx
@@ -1,8 +1,18 @@
 export default function RootLayout({
   children,
+  params,
 }: {
   children: React.ReactNode;
+  params: {
+    id: string;
+  };
 }) {
+  let appName = "Swap";
+  let titleSuffix = " - Powered by DODO protocol";
+  if (params.id && /^[a-zA-Z0-9_]*$/.test(params.id)) {
+    appName = params.id;
+  }
+  const title = appName + titleSuffix;
   return (
     <html lang="en">
       <head>
@@ -10,6 +20,7 @@ export default function RootLayout({
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
+        <title>{title}</title>
       </head>
       <body>{children}</body>
     </html>
